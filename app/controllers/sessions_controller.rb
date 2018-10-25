1178671866e982873b4a8189b6a7ead9f9461d35
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-25 20:30:06 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     @service = GithubService.new
     session[:token] = @service.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
     session[:username] = @service.get_username
+
     redirect_to '/'
   end
 end
