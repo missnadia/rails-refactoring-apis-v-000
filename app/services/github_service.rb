a41feae6fb2afed286f9c9bb53aea46c87ea6dad
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-25 20:53:46 +0000

    Done.

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -27,6 +27,6 @@ class GithubService
   end
 
   def create_repo(name)
-    response = Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
   end
 end
