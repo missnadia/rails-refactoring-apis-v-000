6434342c907f2b047a18c87c5d7997a97992128c
Nadia Lee <miss.nadia.lee@gmail.com>
2018-10-28 19:12:36 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,10 @@
 class RepositoriesController < ApplicationController
   def index
+<<<<<<< HEAD
     @service = GithubService.new(@access_token)
+=======
+    @service = GithubService.new(session[:token])
+>>>>>>> 7ff09cfc9b06c915108a5b741a9a40c572953782
     @repos_array = @service.get_repos
   end
 
